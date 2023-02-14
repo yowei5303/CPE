c71bc47e055be75ff323e59e0d6cb65d94e985d2
yowei_L <70736964+yowei5303@users.noreply.github.com>
2023-02-14 17:26:49 +0800

    Add files via upload

--- 10336_Rank_the_Languages.py
+++ 10336_Rank_the_Languages.py
@@ -19,6 +19,12 @@ area = [
     [[2,3],[2,3],[0,1,4,5],[0,1,4,5],] ##u
     [[6,7],[6,7],[6,7],[6,7]]         ##d
     ]
+area = [
+    [[0,1,4,5],[2,3],[6,7]],
+    [[0,1,4,5],[2,3],[6,7]], 
+    [[2,3],[0,1,4,5],[6,7]].
+    [[2,3],[0,1,4,5],[6,7]].
+    ]
 然後再判斷第一次的數據:
     [0,1]連續所以t範圍+1，[2,3]連續所以u範圍+1，已次類推。
 第二次:
@@ -28,21 +34,40 @@ area = [
     [2,3]連續而且在這之前的u陣列沒出現過，所以+1。
     [0,1]連續而且在這之前的u陣列沒出現過，所以+1。
 '''
-Data_quantity = input()
+Data_quantity = int(input())
 Letter = []
-area = [[]]
+
+area = [[[]]]
 
 for i in range(Data_quantity):
     Rows,Size = map(int,input().split())
 
     for j in range(Rows):
         str = input()
-        if(str[j] not in area):
-            Letter.append(str[j])
-            area[Letter.index(str[j])].append(j) 
-        elif(str[j] in Letter):
-            area[Letter.index(str[j])].append(j)
+        #print("A",area)
+        #print("B",Letter)
+        #area[j].append([])
+        for k in range(len(Letter)):
+            area[j].append([])
+
+        for k in range(Size):
+            
+            #print(str[k] not in Letter)
+            if(str[k] not in Letter):
+                Letter.append(str[k])
+                ##print(Letter.index(str[k]))
+                area[j].append([])
+                area[j][Letter.index(str[k])].append(k) 
+            elif(str[k] in Letter):
+                try:
+                    area[j][Letter.index(str[k])].append(k)
+                except IndexError:
+                    area[j].append([])
+                    area[j][Letter.index(str[k])].append(k)
+        area.append([])
+
+        
 
 
-print()
+print(area)
             
