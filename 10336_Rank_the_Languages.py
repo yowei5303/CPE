98bbd49664b8bbc8484bdbfe0eb7c54f6e4ff6ba
yowei5303 <yliu13548@gmail.com>
2023-02-14 22:17:47 +0800

    yeee

--- 10336_Rank_the_Languages.py
+++ 10336_Rank_the_Languages.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 1
 4 8
@@ -7,38 +6,44 @@ ttuuttdd
 uuttuudd
 uuttuudd
 '''
-
 '''
-我的想法:
-
-首先將出現的字母，存放進Letter裡面。
-用意是可以將位置的數據存在同意維度。
-例:
-Letter = [t,u,d]
-area = [
-    [[0,1,4,5],[0,1,4,5],[2,3],[2,3],] ##t
-    [[2,3],[2,3],[0,1,4,5],[0,1,4,5],] ##u
-    [[6,7],[6,7],[6,7],[6,7]]         ##d
-    ]
-area = [
-    [[0,1,4,5],[2,3],[6,7]],
-    [[0,1,4,5],[2,3],[6,7]], 
-    [[2,3],[0,1,4,5],[6,7]].
-    [[2,3],[0,1,4,5],[6,7]].
-    ]
-然後再判斷第一次的數據:
-    [0,1]連續所以t範圍+1，[2,3]連續所以u範圍+1，已次類推。
-第二次:
-    [0,1]連續但是前一個陣列已經出現過，所以pass。
-    [2,3]連續但是前一個陣列已經出現過，所以pass。
-第三次:
-    [2,3]連續而且在這之前的u陣列沒出現過，所以+1。
-    [0,1]連續而且在這之前的u陣列沒出現過，所以+1。
+先擱著
 '''
 Data_quantity = int(input())
 Letter = []
-
 area = [[[]]]
+mapp = []
+#str = []
+
+for i in range(Data_quantity):
+    Rows,Size = map(int,input().split())
+    
+    mapplist = []
+    for j in range(Rows):
+        mapp.append([])
+        mapp[j] = input()
+        mapplist.append([])
+        mapplist[y] = list(mapp[y])
+    
+
+    for y in range(Rows):
+        for x in range(Size):
+            if(mapp[y][x] not in Letter):
+                Letter.append(mapp[y][x])
+                mapplist[y][x] = Letter.index(mapp[y][x])
+            elif(mapp[y][x] in Letter):
+                mapplist[y][x] = Letter.index(mapp[y][x])
+            #print(Letter.index(mapp[y][x]))
+    
+
+    print(mapplist)
+                
+
+
+
+
+
+'''
 
 for i in range(Data_quantity):
     Rows,Size = map(int,input().split())
@@ -71,79 +76,5 @@ for i in range(Data_quantity):
 
 
 print(area)
+'''
             
-=======
-'''
-1
-4 8
-ttuuttdd
-ttuuttdd
-uuttuudd
-uuttuudd
-'''
-
-'''
-我的想法:
-
-首先將出現的字母，存放進Letter裡面。
-用意是可以將位置的數據存在同意維度。
-例:
-Letter = [t,u,d]
-area = [
-    [[0,1,4,5],[0,1,4,5],[2,3],[2,3],] ##t
-    [[2,3],[2,3],[0,1,4,5],[0,1,4,5],] ##u
-    [[6,7],[6,7],[6,7],[6,7]]         ##d
-    ]
-area = [
-    [[0,1,4,5],[2,3],[6,7]],
-    [[0,1,4,5],[2,3],[6,7]], 
-    [[2,3],[0,1,4,5],[6,7]].
-    [[2,3],[0,1,4,5],[6,7]].
-    ]
-然後再判斷第一次的數據:
-    [0,1]連續所以t範圍+1，[2,3]連續所以u範圍+1，已次類推。
-第二次:
-    [0,1]連續但是前一個陣列已經出現過，所以pass。
-    [2,3]連續但是前一個陣列已經出現過，所以pass。
-第三次:
-    [2,3]連續而且在這之前的u陣列沒出現過，所以+1。
-    [0,1]連續而且在這之前的u陣列沒出現過，所以+1。
-'''
-Data_quantity = int(input())
-Letter = []
-
-area = [[[]]]
-
-for i in range(Data_quantity):
-    Rows,Size = map(int,input().split())
-
-    for j in range(Rows):
-        str = input()
-        #print("A",area)
-        #print("B",Letter)
-        #area[j].append([])
-        for k in range(len(Letter)):
-            area[j].append([])
-
-        for k in range(Size):
-            
-            #print(str[k] not in Letter)
-            if(str[k] not in Letter):
-                Letter.append(str[k])
-                ##print(Letter.index(str[k]))
-                area[j].append([])
-                area[j][Letter.index(str[k])].append(k) 
-            elif(str[k] in Letter):
-                try:
-                    area[j][Letter.index(str[k])].append(k)
-                except IndexError:
-                    area[j].append([])
-                    area[j][Letter.index(str[k])].append(k)
-        area.append([])
-
-        
-
-
-print(area)
-            
->>>>>>> c71bc47e055be75ff323e59e0d6cb65d94e985d2
--- 10789_PrimeFrequency.py
+++ 10789_PrimeFrequency.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ##10789 Prime Frequency
 
 
@@ -50,56 +49,3 @@ for i in range(T) :
 
 
 
-=======
-##10789 Prime Frequency
-
-
-
-## 函式解說
-def Isprime(N):
-    if (N > 2):
-        if (N % 2 == 0): ##質數絕對不可能被2給整除
-            return False
-        for i in range(3,int((N ** (0.5)))+1,2): ##根據埃拉托斯特尼篩法，迴圈不用計算到N-1，只需要到平方根N就好
-            if (N % i == 0) :
-                return False
-        return True
-    elif (N == 2):
-        return True
-    else:
-        return False
-    
-
-T = int(input())
-empty = 0
-out = ""
-for i in range(T) :
-    str = input()
-    out = ""
-    for j in range(9):
-        num = str.count(chr(48+j))
-        if (Isprime(num)):
-            #print("case ",i+1,":",chr(48+j))
-            out += chr(48+j)
-    for j in range(26):
-        capital = str.count(chr(65+j))
-        if (Isprime(capital)):
-            ##print("case ",i+1,":",chr(65+j))
-            out += chr(65+j)
-    for j in range(26):
-        lower = str.count(chr(97+j))
-        if (Isprime(lower)):
-            #print("case ",i+1,":",chr(97+j))
-            out += chr(97+j)
-    if len(out) == 0 :
-        print("case ",i+1,":","empty")
-    else:
-        print("case ",i+1,":",out)
-    
-
-
-
-
-
-
->>>>>>> c71bc47e055be75ff323e59e0d6cb65d94e985d2
